f38cd8c153dba743322066cfbfaba813a736caa1
Ross Roberts <ross123roberts@gmail.com>
2021-03-09 20:40:03 -0500

    chapter 7 exercises

--- chapter7.scala
+++ chapter7.scala
@@ -44,3 +44,62 @@ def nextFib(x: Long): Option[Long] = {
 nextFib(9)
 
 //2)
+
+def listDir(dir: String): List[String] = {
+  val filesRaw = new java.io.File(dir).listFiles
+  val files = filesRaw.map(_.getName).filterNot(_.startsWith("."))
+  files.toList
+}
+
+val files = listDir("/Users/RossRoberts/")
+
+//3)
+val filesOrder = files.groupBy(_.head.toLower).toList.sortBy(_._1)
+
+
+//4)
+
+def toDouble(x: String): Option[Double] = util.Try(x.toDouble).toOption
+
+def prod(x: String, y: String): Option[Double] = {
+  (toDouble(x), toDouble(y)) match {
+    case (Some(a), Some(b)) => Some(a*b)
+    case _ => None
+  }
+}
+
+prod("10", "9.1")
+
+//5)
+def safeGetProperty(s: String): Option[String] = {
+  util.Try( System.getProperty(s)) match {
+    case util.Success(x) => Option(x)
+    case util.Failure(ex) => None
+  }
+}
+
+safeGetProperty(null)
+safeGetProperty("os.arch")
+safeGetProperty("blah")
+
+//6)
+def xmlParse(xml: String) = xml.split("</?entry>").filterNot(_.isEmpty).toList
+
+def getChild(xml: String, name: String): Option[String] = {
+  val p = s".*<$name>(.*)</$name>.*".r
+  xml match {
+    case p(result) => Option(result)
+    case _ => None
+  }
+}
+
+def getGithubRss(user: String, repo: String, branch: String): String = {
+  val url = s"https://github.com/$user/$repo/commits/$branch.atom"
+  val s = io.Source.fromURL(url)
+  val text = s.getLines.map(_.trim).mkString("")
+  text
+}
+
+val xml = getGithubRss("scala", "scala", "2.11.x")
+
+val firstTitle = getChild(xml, "title")
