d16f50e842b413a37a9686ae268f9337302b22ee
Ross Roberts <ross123roberts@gmail.com>
2021-03-09 21:07:55 -0500

    chapter 8 exercises

---
+++ chapter8.scala
@@ -0,0 +1,6 @@
+//1)
+//a. Create a consolce class that can track the male, model, debut date, wifi type, physical media formats sipported and max video res.
+//override defailt toString.
+
+class Console(make: String, model: String,  )
+}
