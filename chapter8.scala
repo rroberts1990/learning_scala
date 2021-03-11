fa0c741f85e2908718e42b99b9572fe30e32891e
Ross Roberts <ross123roberts@gmail.com>
2021-03-11 07:33:22 -0500

    chapter 8 exercises

--- chapter8.scala
+++ chapter8.scala
@@ -1,6 +1,45 @@
-//1)
-//a. Create a consolce class that can track the male, model, debut date, wifi type, physical media formats sipported and max video res.
-//override defailt toString.
+package chapter8
 
-class Console(make: String, model: String,  )
+import java.util.Date
+
+//1)
+//a. Create a console class that can track the male, model, debut date, wifi type, physical media formats supported and max video res.
+//override default toString.
+
+abstract class MediaFormat
+class Dvd extends MediaFormat
+class BD extends MediaFormat
+class Digital extends MediaFormat
+class Usb extends MediaFormat
+
+abstract class Resolution(pixels: Int)
+class HD extends VideoResolution(1280 * 720)
+class FHD extends VideoResolution(1920 * 1080)
+class QHD extends VideoResolution(2560 * 1440)
+class UHD4K extends VideoResolution(3840 * 2160)
+
+class GamingConsole(val make: String, val model: String, val debutDate: Date, val Wifi: Option[String],
+                    val media: List[MediaFormat], val resolution: Resolution) {
+  override def toString = {s"$mk: $mod \n Released in: $dd \n WiFi type: $w\n Supports the following: $med"}
+}
+
+class ConsoleGen {
+  def strToDate(d: String) = Date.valueOf(d)
+
+  val ps5 = new GamingConsole("Playstation", "5", strToDate("2021-01-01"), Some("a/b"),
+    List(new Dvd, new Usb), new HD)
+
+  val aquaTopia = new GameConsole("Dreamcast", "Whatever", strToDate("2012-01-01"), Some("a/b/g"),
+    List(new DvdMediaFormat), new HD)
+
+  val oonaSeven = new GameConsole("Xbox", "360", strToDate("2014-01-01"), Some("b/g/n"),
+    List(new BluRayMediaFormat, new DvdMediaFormat), new FHD)
+
+  val leoChallenge = new GameConsole("Sega", "Megadrive", strToDate("2015-01-01"), Some("g/n"),
+    List(new USBMediaFormat), new UHD4K)
+}
+
+class Game(val name: String, val producer: String, val consoles: List[GamingConsole]) {
+  def isSupported(x: GamingConsole) = consoles.contains(x)
+  override def toString = s"Game($name, by $maker)"
 }
